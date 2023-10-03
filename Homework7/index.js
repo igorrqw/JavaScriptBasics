1614d77069429ad8a9a4c49d14af78198ee9740c
Василенко Игорь <warface.777@mail.ru>
2023-10-03 18:19:53 +0300

    Homework7

---
+++ Homework7/index.js
@@ -0,0 +1,41 @@
+/*Задание 1: "Управление библиотекой книг"
+Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
+Свойство title (название) - строка, название книги.
+Свойство author (автор) - строка, имя автора книги.
+Свойство pages (количество страниц) - число, количество страниц в книге.
+Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).*/
+
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+  displayInfo = () =>
+    console.log(
+      `Book: "${this.title}", Author: "${this.author}", Pages: ${this.pages}`
+    );
+}
+
+const book = new Book("BookName1", "Author1", 123);
+book.displayInfo();
+
+/*Задание 2: "Управление списком студентов"
+Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
+Свойство name (имя) - строка, имя студента.
+Свойство age (возраст) - число, возраст студента.
+Свойство grade (класс) - строка, класс, в котором учится студент.
+Метод displayInfo() - выводит информацию о студенте в консоль.*/
+
+class Student {
+  constructor(name, age, grade) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+  displayInfo = () =>
+    console.log(`Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`);
+}
+
+const student = new Student("John Smith", 16, 10);
+student.displayInfo();
